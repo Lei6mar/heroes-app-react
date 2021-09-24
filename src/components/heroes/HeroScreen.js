dad1c1874b28a95601c20f4e0a71eeb90da9fb17
miledesmam <miledesmam@gmail.com>
2021-09-23 19:19:40 -0500

    WIP

--- src/components/heroes/HeroScreen.js
+++ src/components/heroes/HeroScreen.js
@@ -1,6 +1,21 @@
 import React from 'react'
+import { Redirect } from 'react-router-dom';
+import { useParams } from 'react-router-dom'
+import { getHeroById } from '../../selectors/getHeroById';
 
 export const HeroScreen = () => {
+  const {heroeid} = useParams();
+  const hero = getHeroById(heroeid)
+  if(!hero){
+    return <Redirect to="/"/>
+  }
+  const {
+    id,
+  superhero,
+  alter_ego,
+  first_appearance,
+  characters,
+  } = hero
   return (
     <div>
       <h1>HeroScreen</h1>
